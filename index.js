3d40091039f160ff07c33d9e1fef07a63e1e0271
Mary Rose Cook <mary@maryrosecook.com>
2016-02-14 09:37:12 +0100

    generate bytecode for conditionals, forever, lambdas and invocations

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var ANNOTATE = "annotate";
 var DO_NOT_ANNOTATE = "do_not_annotate";
 
 var semantics = grammar.semantics().addOperation("bytecode", {
+
+
+  // TODO: any missing rules?
+
+
   ExpressionList: function(listOf) {
     var el = this;
     var expressions = listOf.extractListOf();
@@ -22,13 +27,81 @@ var semantics = grammar.semantics().addOperation("bytecode", {
     return e.bytecode();
   },
 
-  Assignment: function(identifier, _colon, value) {
+  Assignment: function(name, _colon, value) {
     return value.bytecode()
-      .concat(ins(["set_env", identifier.bytecode()], this, ANNOTATE));
+      .concat(ins(["set_env", name.bytecode()], this, ANNOTATE));
+  },
+
+  Invocation: function(invocable, _brace, argList, _brace) {
+    // TODO: port across TCO optimisation
+
+    var args = argList.extractListOf();
+    return ins(["arg_start"], argList)
+      .concat(mapCat(args,
+                     function(a) { return a.bytecode(); }))
+      .concat(invocable.bytecode())
+      .concat(ins(["invoke", args.length], this, ANNOTATE));
+  },
+
+  Lambda: function(_openBrace, parameterList, expressionList, _closeBrace) {
+    var parameters = parameterList.extractListOf()
+        .map(function(p) { return p.interval.contents.slice(1); });
+
+    return ins(["push_lambda", { bc: expressionList.bytecode(), parameters: parameters }],
+               this,
+               ANNOTATE);
+  },
+
+  Block: function(lambda) {
+    return ins(["arg_start"], this)
+      .concat(lambda.bytecode())
+      .concat(ins(["invoke", 0], this, DO_NOT_ANNOTATE));
+  },
+
+  Forever: function(_foreverKeyword, block) {
+    return block.bytecode()
+      .concat(ins(["pop"], this),
+              ins(["jump", -4], this));
+  },
+
+  Conditional: function(ifStatement, elseifStatements, elseStatement) {
+    return [ifStatement]
+      .concat(elseifStatements.children)
+      .concat(elseStatement.children)
+      .map(function(s) { return s.bytecode(); })
+      .concat([ins(["push", undefined], this)]) // terminal else in case no clauses run
+      .reverse()
+      .reduce(function(bc, clause) { // jump to end of conditional if a clause runs
+        return clause
+          .concat(ins(["jump", bc.length], ifStatement))
+          .concat(bc);
+      }, []);
+  },
+
+  If: function(_ifKeyword, conditionalExpression, block) {
+    return conditionalExpression.bytecode()
+      .concat(ins(["if_not_true_jump", 4], this))
+      .concat(block.bytecode());
+  },
+
+  Elseif: function(_elseifKeyword, conditionalExpression, block) {
+    return conditionalExpression.bytecode()
+      .concat(ins(["if_not_true_jump", 4], this))
+      .concat(block.bytecode());
+  },
+
+  Else: function(_elseKeyword, block) {
+    return ins(["push", true], this, ANNOTATE)
+      .concat(ins(["if_not_true_jump", 4], this))
+      .concat(block.bytecode());
   },
 
   Literal: function(l) {
     return ins(["push", l.bytecode()], this, ANNOTATE);
+  },
+
+  identifierLookup: function(identifier) {
+    return ins(["get_env", identifier.bytecode()], this, ANNOTATE);
   },
 
   identifier: function(firstLetter, restCharacters) {
@@ -37,6 +110,10 @@ var semantics = grammar.semantics().addOperation("bytecode", {
 
   number: function(_, _, _, _) {
     return parseFloat(this.interval.contents, 10);
+  },
+
+  boolean: function(keyword) {
+    return keyword.interval.contents === "true" ? true : false;
   },
 
   listOf: function(l) {
--- spec/bytecode.spec.js
+++ spec/bytecode.spec.js
@@ -35,6 +35,253 @@ describe("bytecode compiler", function() {
                   ["return"]]);
     });
   });
+
+  describe("invocations", function() {
+    it("should compile an invocation with no args", function() {
+      expect(stripBc(bytecode("a1()")))
+        .toEqual([["arg_start"],
+                  ["get_env", "a1"],
+                  ["invoke", 0],
+                  ["return"]]);
+    });
+
+    it("should compile an invocation with two args", function() {
+      expect(stripBc(bytecode("a1(2 b3)")))
+        .toEqual([["arg_start"],
+                  ["push", 2],
+                  ["get_env", "b3"],
+                  ["get_env", "a1"],
+                  ["invoke", 2],
+                  ["return"]]);
+    });
+  });
+
+  describe("lambdas", function() {
+    it("should compile an empty lambda", function() {
+      expect(stripBc(bytecode("{}")))
+        .toEqual([["push_lambda", { bc: [["push", undefined],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+
+    it("should compile an empty lambda with some parameters", function() {
+      expect(stripBc(bytecode("{ ?a1 ?b2 }")))
+        .toEqual([["push_lambda", { bc: [["push", undefined],
+                                         ["return"]],
+                                    parameters: ["a1", "b2"]}],
+                  ["return"]]);
+    });
+
+    it("should compile a lambda that returns 1", function() {
+      expect(stripBc(bytecode("{ 1 }")))
+        .toEqual([["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+
+    it("should compile lambda that has 2 expressions and returns the second", function() {
+      expect(stripBc(bytecode("{ 1 \n 2 }")))
+        .toEqual([["push_lambda", { bc: [["push", 1],
+                                         ["push", 2],
+                                         ["pop"],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+
+    it("should compile lambda that contains an invocation on no arguments", function() {
+      expect(stripBc(bytecode("{ a1() }")))
+        .toEqual([["push_lambda", { bc: [["arg_start"],
+                                         ["get_env", "a1"],
+                                         ["invoke", 0],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+
+    it("should compile lambda that contains an invocation with some arguments", function() {
+      expect(stripBc(bytecode("{ a1(2 3) }")))
+        .toEqual([["push_lambda", { bc: [["arg_start"],
+                                         ["push", 2],
+                                         ["push", 3],
+                                         ["get_env", "a1"],
+                                         ["invoke", 2],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+
+    it("should compile invocation of lambda literal", function() {
+      expect(stripBc(bytecode("{ {}() }")))
+        .toEqual([["push_lambda", { bc: [["arg_start"],
+                                         ["push_lambda", { bc: [["push", undefined],
+                                                                ["return"]],
+                                                           parameters: [] }],
+                                         ["invoke", 0],
+                                         ["return"]],
+                                    parameters: []}],
+                  ["return"]]);
+    });
+  });
+
+  describe("forever", function() {
+    it("should not annotate invoke", function() {
+      var bc = bytecode("forever {}");
+      expect(bc[2][0]).toEqual("invoke");
+      expect(bc[2].annotate).toEqual(false);
+    });
+  });
+
+  describe("conditionals", function() {
+    it("should compile an if", function() {
+      expect(stripBc(bytecode("if true { 1 }")))
+        .toEqual([["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 2],
+
+                  ["push", undefined],
+                  ["jump", 0],
+
+                  ["return"]]);
+    });
+
+    it("should compile an if/else", function() {
+      expect(stripBc(bytecode("if true { 1 } else { 2 }")))
+        .toEqual([["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 8],
+
+                  ["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 2],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 2],
+
+                  ["push", undefined],
+                  ["jump", 0],
+
+                  ["return"]]);
+    });
+
+    it("should compile an if/elseif/else", function() {
+      expect(stripBc(bytecode("if true { 1 } elseif false { 2 } else { 3 }")))
+        .toEqual([["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 14],
+
+                  ["push", false],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 2],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 8],
+
+                  ["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 3],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 2],
+
+                  ["push", undefined],
+                  ["jump", 0],
+
+                  ["return"]]);
+    });
+
+    it("should compile an if/elseif/elseif/else", function() {
+      expect(stripBc(bytecode("if true { 1 } elseif false { 2 } elseif true { 3 } else { 4 }")))
+        .toEqual([["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 20],
+
+                  ["push", false],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 2],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 14],
+
+                  ["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 3],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 8],
+
+                  ["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 4],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 2],
+
+                  ["push", undefined],
+                  ["jump", 0],
+
+                  ["return"]]);
+    });
+
+    it("should compile an if/elseif/elseif/else", function() {
+      expect(stripBc(bytecode("if true { 1 } elseif false { 2 }")))
+        .toEqual([["push", true],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 1],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 8],
+                  ["push", false],
+                  ["if_not_true_jump", 4],
+                  ["arg_start"],
+                  ["push_lambda", { bc: [["push", 2],
+                                         ["return"]],
+                                    parameters: [] }],
+                  ["invoke", 0],
+                  ["jump", 2],
+
+                  ["push", undefined],
+                  ["jump", 0],
+
+                  ["return"]]);
+    });
+  });
 });
 
 function stripBc(bc) {
@@ -43,8 +290,6 @@ function stripBc(bc) {
     delete instruction.end;
     delete instruction.annotate;
     if (instruction[0] === "push_lambda") {
-      delete instruction[1].type;
-      delete instruction[1].parameters;
       delete instruction[1].annotate;
       stripBc(instruction[1].bc);
     }
